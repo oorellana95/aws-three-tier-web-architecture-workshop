04d0fe18f24e41c1512ee867830eee00e1a20374
oorellana95 <oorellana30.95@gmail.com>
2023-08-19 19:30:52 -0500

    add app tier and web tier code

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'aws-three-tier-db.cluster-cezpmbmaliwd.us-west-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Candanxu95',
+    DB_DATABASE : 'webappdb'
+});
